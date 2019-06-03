77ea0d291834857c06e831c2c3acb4b8e2853c61
Torvald Lekvam <117844+torvald@users.noreply.github.com>
2019-06-03 22:32:23 +0200

    Parse starred documents, and documents in (starred) folders

--- quipdiff.py
+++ quipdiff.py
@@ -1,6 +1,7 @@
 import difflib
 import os
 import quip
+import time
 from bs4 import BeautifulSoup
 
 import config
@@ -8,7 +9,10 @@ from utils.mail import send_mail, html_enclose, html_colorize_diff
 
 token = config.token
 state_dir = config.state_dir
+diff_starred = config.diff_starred
 threads = config.threads
+folder_ids = config.folder_ids
+thread_ids = config.thread_ids
 
 output_format = config.output_format
 
@@ -18,6 +22,7 @@ user = client.get_authenticated_user()
 
 def get_thread(thread_id):
     q = client.get_thread(thread_id)
+    link = q["thread"]["link"]
     title = q["thread"]["title"]
     html = q.get("html")
     parsed_html = BeautifulSoup(html, "html.parser")
@@ -33,7 +38,7 @@ def get_thread(thread_id):
     else:
         raise Exception("no valid output_format in settings")
 
-    return title, content
+    return title, content, link
 
 
 def get_thread_content_from_state(thread_id):
@@ -64,11 +69,41 @@ def extract_thread_ids(url):
     # https://kolonialno.quip.com/O14xAEcqbOeT/Fisk-og-andre-buskevekster
     return url.split("/")[3]
 
+# TODO: clean this up, make a main() and dry up a bit
+if diff_starred:
+    starred = client.get_folder(user["starred_folder_id"])
+    for star in starred["children"]:
+        if "folder_id" in star:
+            folder_ids.append(star["folder_id"])
+        if "thread_id" in star:
+            thread_ids.append(star["thread_id"])
+
+for folder_id in folder_ids:
+    folder = client.get_folder(folder_id)
+    for item in folder["children"]:
+        if "thread_id" in item:
+            thread_ids.append(item["thread_id"])
 
 for url in threads:
     # Extract thread_id and content of current version in Quip
-    thread_id = extract_thread_ids(url)
-    thread_title, thread_content_now = get_thread(thread_id)
+    thread_ids.append(extract_thread_ids(url))
+
+rate_limit_trottle_time = 0
+
+if len(thread_ids) > 50:
+    # Since quip rate limits 50 request per minute, we need to ensure we dont
+    # exceed that by adding two seconds between every request
+    rate_limit_trottle_time = 2
+
+if len(thread_ids) > 900:
+    # If you run this script at every hour (which is what I would recommend),
+    # you can not request more the 900 documents, as this is the rate limit per
+    # hour. You should propably not reach this limit anyway.
+    raise Exception("Nope, got to many docs to parse ({})".format(len(thread_ids)))
+
+for thread_id in thread_ids:
+    time.sleep(rate_limit_trottle_time)
+    thread_title, thread_content_now, thread_link = get_thread(thread_id)
 
     # If we have never seen the document before, save it to state and continue
     # to next document
@@ -92,7 +127,7 @@ for url in threads:
             password = config.smtp_password
             smtp_recipient = config.smtp_recipient
             subject = "Quipdiff in {}".format(thread_title)
-            body_plain = " ***** {} ***** \n\n {}".format(url, diff_output)
+            body_plain = " ***** {} ***** \n\n {}".format(thread_link, diff_output)
             body_html = html_enclose(html_colorize_diff(body_plain))
             send_mail(
                 username, password, smtp_recipient, subject, body_plain, body_html
@@ -101,5 +136,5 @@ for url in threads:
         else:
             # stderr
             print(" ***** {} ***** ".format(thread_title))
-            print(" ***** {} ***** ".format(url))
+            print(" ***** {} ***** ".format(thread_link))
             print("\n" + diff_output + "\n")
